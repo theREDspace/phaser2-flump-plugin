2434d22c1b6750dfd63156040970a46740660b97
amalley <aaron.malley@redspace.com>
2019-09-26 16:28:50 -0300

    no message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import 'phaser-ce';
-
-export class FlumpPlugin extends Phaser.Plugin {
-    constructor(game, parent) {
-        super(game, parent);
-        this.game.flump = this.game.flump || this;
-    }
-}
+export * from './flump';
